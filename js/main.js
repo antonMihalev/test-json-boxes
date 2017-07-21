a280883e2d53501a8e25cb08ecaa0a2e1b31ae00
antonMihalev <mihalev.ant@gmail.com>
2017-07-21 10:59:31 +0300

    fixed next to button - opacity_half

--- js/main.js
+++ js/main.js
@@ -23,39 +23,40 @@ $.getJSON( "js/data.json", function( data ) {
           })(data);
           console.log(items);
 
-          (function addRectangle(items){
-          	for(var i = 0; i< items.length; i++){
-          		var newDiv = document.createElement('div');
-          		newDiv.className = 'rectangle';
-          		var text = "Block:"+ (i+1);
-          		var textNode = document.createTextNode(text);
-          		newDiv.appendChild(textNode);
-          		parent.appendChild(newDiv);
-          		
-          		newDiv.setAttribute('data-brd-width', items[i].order);//---data-attr
+           function addRectangle(items){
+	          	for(var i = 0; i< items.length; i++){
+	          		var newDiv = document.createElement('div');
+	          		newDiv.className = 'rectangle';
+	          		var text = "Block:"+ (items[i].order);
+	          		var textNode = document.createTextNode(text);
+	          		newDiv.appendChild(textNode);
+	          		parent.appendChild(newDiv);
+	          		
+	          		newDiv.setAttribute('data-brd-width', items[i].order);//---data-attr
 
-          		//------------------ORDER as BORDER START
-          		newDiv.style.borderWidth = items[i].order+'px';
-          		//------------------ORDER as BORDER END
+	          		//------------------ORDER as BORDER START
+	          		newDiv.style.borderWidth = items[i].order+'px';
+	          		//------------------ORDER as BORDER END
 
-          		//------------------ODD or EVEN as BORDER or BackGround START
-          		if(items[i].order % 2){
-          			newDiv.style.backgroundColor = items[i].color;
-          			newDiv.setAttribute('data-color', items[i].color);//---data-attr
-          		}else {
-          			newDiv.style.borderColor = items[i].color;
-          			newDiv.setAttribute('data-color', items[i].color);//---data-attr
-          		}
-          		//------------------ODD or EVEN as BORDER or BackGround END
+	          		//------------------ODD or EVEN as BORDER or BackGround START
+	          		if(items[i].order % 2){
+	          			newDiv.style.backgroundColor = items[i].color;
+	          			newDiv.setAttribute('data-color', items[i].color);//---data-attr
+	          		}else {
+	          			newDiv.style.borderColor = items[i].color;
+	          			newDiv.setAttribute('data-color', items[i].color);//---data-attr
+	          		}
+	          		//------------------ODD or EVEN as BORDER or BackGround END
 
-          		if((i % 3) === 0) {newDiv.classList.add("clearfix")} //Each fouth start new line
+	          		if((i % 3) === 0) {newDiv.classList.add("clearfix")} //Each fouth start new line
 
-          		if(((i+1) % 2) === 0) {newDiv.classList.add("box_shadow")} //Odd has shadow
+	          		if(((i+1) % 2) === 0) {newDiv.classList.add("box_shadow")} //Odd has shadow
 
-          		// console.log(items[i]);
-          	}
+	          		// console.log(items[i]);
+	          	}
 
-          })(items);
+	          }
+	          addRectangle(items);
 
           //------------------HOVER EFFECT current-Rotate, prev-Yellow, next-Opacity 0.5
           	document.body.onmouseover = document.body.onmouseout = document.body.onclick = document.body.ondblclick= handler;
@@ -75,7 +76,7 @@ $.getJSON( "js/data.json", function( data ) {
 				    if(event.target.previousElementSibling.classList != '' && !event.target.hasAttribute('data-btn')){
 				    	event.target.previousElementSibling.style.backgroundColor = 'yellow';
 				    }
-				    if(event.target.nextElementSibling.classList.contains('rectangle')){
+				    if(event.target.nextElementSibling.classList.contains('rectangle') && !event.target.hasAttribute('data-btn')){
 				    	event.target.nextElementSibling.classList.add('opacity_half');
 				    }
 				    	
@@ -88,17 +89,27 @@ $.getJSON( "js/data.json", function( data ) {
 				    	event.target.previousElementSibling.style.borderColor = event.target.previousElementSibling.getAttribute('data-color');
 				    	event.target.previousElementSibling.style.backgroundColor = 'initial';
 				    } 
-			
-				    event.target.nextElementSibling.classList.remove('opacity_half');
+				    if(event.target.nextElementSibling.classList.contains('rectangle')){
+				    	event.target.nextElementSibling.classList.remove('opacity_half');
+				    }
 				}		
 
 				  //---------Click => border-x-3. Second Click => return to initial view
 			  if (event.type == 'click') {
-				    event.target.style.borderWidth = (event.target.getAttribute('data-brd-width')*3)+'px'; 
+			  	if( event.target != '[object HTMLButtonElement]' ){
+				    event.target.style.borderWidth = (event.target.getAttribute('data-brd-width')*3)+'px';
+			  	}
+			  	if( event.target == '[object HTMLButtonElement]' ){
+				    addRectangle(sortedItems);
+			  	}
 			  }
 			  if (event.type == 'dblclick') {
 				    event.target.style.borderWidth = event.target.getAttribute('data-brd-width')+'px'; 
+			  		/*if( event.target == '[object HTMLButtonElement]' ){
+				    	addRectangle(items);
+			  		}*/
 			  }
+
 			}
 	        //------------------HOVER EFFECT END
 
